64860b75c0ae5973a2cca21e5e80541223017732
KevinD <kevduong47@gmail.com>
2022-08-02 13:52:49 -0400

    added seed data

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const { setTokenCookie, requireAuth } = require('../../utils/auth')
+const { Spot } = require('../../db/models')
 const router = express.Router();
 
 // Get all Spots
 router.get('/', async (req, res) => {
-    
+    const allSpots = await Spot.findAll({
+
+    })
+    return res.json(allSpots)
 })
 
-router.get('/')
+modules.exports = router

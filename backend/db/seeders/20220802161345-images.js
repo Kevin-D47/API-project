ec937099474033572c62c4018dbaa337c7ce4a75
KevinD <kevduong47@gmail.com>
2022-08-04 11:19:01 -0400

    changes

--- backend/db/seeders/20220802161345-images.js
+++ backend/db/seeders/20220802161345-images.js
@@ -4,21 +4,21 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Images', [
       {
-        url: 'wwww.house1.com/download.jpeg',
+        url: 'wwww.house1.com',
         previewImage: true,
         spotId: 1,
         reviewId: null,
         userId: 1
       },
       {
-        url: 'wwww.house2.com/download.jpeg',
+        url: 'wwww.house2.com',
         previewImage: false,
         spotId: null,
         reviewId: 2,
         userId: 2
       },
       {
-        url: 'wwww.apartment1.com/download.jpeg',
+        url: 'wwww.apartment1.com',
         previewImage: true,
         spotId: 3,
         reviewId: null,

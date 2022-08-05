4ca6dc53e370f48fb37d916f081f51a1722e04f8
KevinD <kevduong47@gmail.com>
2022-08-05 11:41:24 -0400

    ready for heroku test

--- backend/db/seeders/20220802161246-bookings.js
+++ backend/db/seeders/20220802161246-bookings.js
@@ -7,19 +7,19 @@ module.exports = {
         userId: 1,
         spotId: 1,
         startDate: new Date("2022-07-18"),
-        enddate: new Date("2022-07-20")
+        endDate: new Date("2022-07-20")
       },
       {
         userId: 2,
         spotId: 2,
         startDate: new Date("2022-10-03"),
-        enddate: new Date("2022-10-09")
+        endDate: new Date("2022-10-09")
       },
       {
         userId: 3,
         spotId: 3,
         startDate: new Date("2022-12-15"),
-        enddate: new Date("2022-12-20"),
+        endDate: new Date("2022-12-20"),
       },
     ], {});
   },

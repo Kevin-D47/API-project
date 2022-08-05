8a7a7d5f83751160d4e85292f6b8889529a0d469
KevinD <kevduong47@gmail.com>
2022-08-05 18:04:46 -0400

    changes

--- backend/db/migrations/20220801214401-create-booking.js
+++ backend/db/migrations/20220801214401-create-booking.js
@@ -11,7 +11,7 @@ module.exports = {
       spotId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: {model: 'Bookings', onDelete: 'CASCADE'}
+        references: {model: 'Spots', onDelete: 'CASCADE'}
       },
       userId: {
         type: Sequelize.INTEGER,

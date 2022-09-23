db8978a22d3d9df239df629bf3ebac081f5d5fc2
KevinD <kevduong47@gmail.com>
2022-09-23 00:41:21 -0400

    refactor

--- frontend/src/components/AllSpots/index.js
+++ frontend/src/components/AllSpots/index.js
@@ -37,7 +37,7 @@ const GetAllSpots = () => {
                                     <div style={{ fontSize: '16px', fontWeight: '600' }}>
                                         {spot.name}
                                     </div>
-                                    <div style={{ fontSize: '14px' }}>
+                                    <div style={{ fontSize: '14px', color: 'dark grey'}}>
                                         {spot.city}, {spot.state}
                                     </div>
                                     <div className='price-container'>
